4b65a3b4517a985571c1f5605795d3deabb0b3e6
Giulia <giuliaparca@gmail.com>
2024-08-06 18:20:47 -0400

    fetching places details based on which keyword user enters

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -8,6 +8,20 @@ function seacrhDestination(){
     fetch('./travel_reccomendation_api.json')
     .then(response => response.json())
     .then ((data)=> {
-        const 
-    })
+        const city = data.countries.cities.find(place => place.name.toLowerCase() === input);
+        const country = data.countries.find(place => place.name.toLowerCase() === input);
+
+        if(city){
+            destinations.innerHTML += `<img src="${city.imagesrc}"></img>`
+            destinations.innerHTML += `<h3> ${city.name}</h3>`;
+            destinations.innerHTML += `<p> ${city.description}</p>`;
+        } else if(country) {
+            destinations.innerHTML += `<h3> ${county.name}</h3>`;
+            destinations.innerHTML += `<p> ${country.cities[0]}</p>`;
+            destinations.innerHTML += `<p> ${country.cities[1]}</p>`;
+        } else {
+            destinations.innerHTML = 'Destination not found. Please, try again.'
+        }
+    });
 }
+        btnSearch.addEventListener('click', searchDestination)
